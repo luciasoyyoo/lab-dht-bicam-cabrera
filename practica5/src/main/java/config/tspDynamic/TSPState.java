75464925eb9ce383f1c04dad10a54292793d404f
Lucía Cabrera <alu0101461106@ull.edu.es>
2025-12-05 17:10:14 +0000

    Comentarios Doxygen en ../config

--- practica5/src/main/java/config/tspDynamic/TSPState.java
+++ practica5/src/main/java/config/tspDynamic/TSPState.java
@@ -1,26 +1,48 @@
 package config.tspDynamic;
 
+/**
+ * TSPState - descripcion (añade detalles).
+ */
 public class TSPState {
 	
 	private int value;
 	private int idCity;
 	//private boolean pool;
 	
+	/**
+	 * getValue - descripcion (añade detalles).
+	 * @return 
+	 */
 	public int getValue() {
 		return value;
 	}
+	/**
+	 * setValue - descripcion (añade detalles).
+	 * @param value 
+	 */
 	public void setValue(int value) {
 		this.value = value;
 	}
-	/*public boolean isPool() {
+	/*
+	public boolean isPool() {
 		return pool;
 	}
+
 	public void setPool(boolean pool) {
 		this.pool = pool;
-	}*/
+	}
+	*/
+	/**
+	 * getIdCity - descripcion (añade detalles).
+	 * @return 
+	 */
 	public int getIdCity() {
 		return idCity;
 	}
+	/**
+	 * setIdCity - descripcion (añade detalles).
+	 * @param idCity 
+	 */
 	public void setIdCity(int idCity) {
 		this.idCity = idCity;
 	}
